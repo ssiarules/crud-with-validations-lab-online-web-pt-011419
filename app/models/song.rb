a3d0545cedd815e6f8c455e248f911f2a8241f6f
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-28 02:46:31 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,16 +1,14 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, presence: true
-  validates :released, inclusion: { in: [true, false] }
-  validates :artist_name, presence: true
+  validates :title, uniqueness:  {
+    scope: [:release_year, :artist_name],
+  }
 
-  with_options if: :released? do |song|
-    song.validates :release_year, presence: true
-    song.validates :release_year, numericality: {
-      less_than_or_equal_to: Date.today.year
-    }
-  end
+  validates :release_year, presence: true, if: :released?
+  validates :release_year, numericality: {only_integer: true, allow_nil: true, less_than_or_equal_to: Time.now.year}
+
   def released?
-    released == true
+    released
   end
+
 end
